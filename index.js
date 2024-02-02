31528a14d51cdc24ee9c71011438292101b41b62
Wong Chang Yeh <changyeh@unicorn.sg>
2024-02-02 14:52:34 +0000

    add get route for clients detail page

--- index.js
+++ index.js
@@ -15,11 +15,40 @@ app.use(express.urlencoded({ extended: false }));
 // Set up Handlebars
 app.set('view engine', 'hbs');
 
+Object.keys(handlebarHelpers).forEach(helperName => {
+    hbs.handlebars.registerHelper(helperName, handlebarHelpers[helperName]);
+});
+
 // Use Wax-On for additional Handlebars helpers
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
+hbs.handlebars.registerHelper('formatDate', function (dateString) {
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    const formattedDate = new Date(dateString).toLocaleDateString('en-US', options);
+    return formattedDate;
+});
+
 async function main(){
+    const connection = await mysql2.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD
+    });
+
+    app.get('/clients', async function (req, res) {
+        // we want the first element from the array returned from connection.execute
+        const [clients] = await connection.execute(`
+            SELECT clients.*, consultants.first_name as c_first_name, consultants.last_name as c_last_name from clients
+                LEFT JOIN consultants ON consultants.consultant_id = clients.consultant_id;
+        `);
+console.log(clients[0]);
+        res.render('clients/index', {
+            clients
+        })
+    });
+
 
 }
 
